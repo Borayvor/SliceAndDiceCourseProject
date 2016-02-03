f4eb0bba5f5923c4b53efa29e62bcc16706c306f
Borayvor <boray@abv.bg>
2016-02-03 20:30:35 +0200

    update menu page

--- MyCuisine_CourseProject/app/js/menu/menu-controller.js
+++ MyCuisine_CourseProject/app/js/menu/menu-controller.js
@@ -1,19 +1,23 @@
 ﻿(function () {
     'use strict';
 
-    function menuController(menuService) {
+    function menuController(menuService, menuCategoryService) {
         var vm = this;
                 
         vm.menuItems = {};
+        vm.menuCategory = {};
 
         menuService.getAll()
+            .then(function (result) {                
+                vm.menuItems = result.results;                
+            });
+
+        menuCategoryService.getAll()
             .then(function (result) {
-                
-                vm.menuItems = result.results;
-                //console.log(vm.menuItems);
+                vm.menuCategory = result.results;
             });
     }
 
     angular.module('MyCuisine.controllers')
-        .controller('MenuController', ['menuService', menuController]);
+        .controller('MenuController', ['menuService', 'menuCategoryService', menuController]);
 }());
